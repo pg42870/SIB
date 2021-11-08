4b03e6fb79c0c2c1a70605346b7fa760f8cc3160
pg42870 <74028296+pg42870@users.noreply.github.com>
2021-11-08 16:44:33 +0000

    add class model with abstract method

---
+++ src/si/supervised/model.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class Model(ABC):
+    def __init__(self):
+        """
+        Abstract class definihg an interface fro supervised learning models
+        """
+        self.is_fitted = False
+
+    @abstractmethod
+    def fit(self, dataset):
+        raise NotImplementedError
+
+    @abstractmethod
+    def predict(self, x):
+        raise NotImplementedError
+
+    @abstractmethod
+    def cost(self):
+        raise NotImplementedError
+
