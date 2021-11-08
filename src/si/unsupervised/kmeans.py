fc9ed4502434f4159e64036fabaa7082ee8c290c
pg42870 <74028296+pg42870@users.noreply.github.com>
2021-11-08 03:26:00 +0000

    update kmeans.py and move to unsurpersived

--- src/si/unsupervised/kmeans.py
+++ src/si/unsupervised/kmeans.py
@@ -1,12 +1,15 @@
 import numpy as np
+from si.util.util import l2_distance
+from copy import copy
+
 
 class KMeans:
 
-    def __init__(self, k: iter, n_iter):
+    def __init__(self, k: iter, n_iter=800):
         self.k = k
         self.max_iter = n_iter
         self.centroids = None
-        #self.distance = l2_distance
+        self.distance = l2_distance
 
     def fit(self, dataset):
         """randomly select k centroids"""
@@ -19,13 +22,15 @@ class KMeans:
         x = dataset.X
         self.centroids = np.array(
             [np.random.uniform(
-                low=self._min[1], high= self._max[i], size=(self.k,)
-            ) for i in range (x.shape[1])]).T
+                low=self._min[1], high=self._max[i], size=(self.k,)
+            ) for i in range(x.shape[1])]).T
+        #rng = np.random.default_rng()
+        #self.centroids = rng.choice(copy(dataset.X), size=self.k, replace=False, p=None, axis=0)
 
     def get_closest_centroid(self, x):
         "retorna o id do centroide mais proximo"
         dist = self.distance(x, self.centroids)
-        closest_centroids_index = np.argmin(dist, axis = 0)
+        closest_centroids_index = np.argmin(dist, axis=0)
         return closest_centroids_index
 
     def transform(self,dataset):
@@ -33,22 +38,24 @@ class KMeans:
         self.init_centroids(dataset)
         print(self.centroids)
         X = dataset.X
-        changed = True
+        changed = False
         count = 0
-        old_idxs= np.zeros(x.shape[0])
-        while changed or count < self.max_iter:
-            idxs = np.apply_along_axis(self.get_closest_centroid(), axis=0, arr=X.T)
+        old_idxs= np.zeros(X.shape[0])
+        while not changed or count < self.max_iter:
+            idxs = np.apply_along_axis(self.get_closest_centroid, axis=0, arr=X.T)
             cent = []
             for i in range(self.k):
-                cent.append(np.mean(X[idxs == i], axis = 0))
-                self.centroids = np.array(cent)
+                cent.append(np.mean(X[idxs == i], axis=0))
+            self.centroids = np.array(cent)
             count += 1
-            changed = np.all(old_idxs==idxs)
+            changed = np.all(old_idxs == idxs)
+            print(changed)
+            print(count)
             old_idxs = idxs
-        return self.centroids, idxs
+        return self.centroids, old_idxs
 
 
-    def fit_transform(self, dataset):
+    def fit_transform(self,dataset):
         self.fit(dataset)
         return self.transform(dataset)
 
