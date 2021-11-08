17bba90f766daaee47f399b9214f98b09cd9acef
pg42870 <74028296+pg42870@users.noreply.github.com>
2021-11-08 16:44:03 +0000

    add metrics with accuracy_score

---
+++ src/si/util/metrics.py
@@ -0,0 +1,9 @@
+
+def accuracy_score(y_true, y_predict):
+
+    correct = 0
+    for true, pred in zip(y_true, y_pred):
+        if true == pred:
+            correct += 1
+    accuracy = correct / len(y_true)
+    return accuracy
