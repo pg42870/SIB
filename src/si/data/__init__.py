00add67216f3cd2a332c68034de10f0a23f38eb5
pg42870 <74028296+pg42870@users.noreply.github.com>
2021-11-29 16:47:34 +0000

    update

--- src/si/data/__init__.py
+++ src/si/data/__init__.py
@@ -1,4 +1,4 @@
 from .dataset import Dataset
-from .util import summary
+from si.util.util import summary
 from .scale import StandardScaler
 from .feature_selection import VarianceThreshold, SelectKBest
