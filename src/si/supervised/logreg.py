3129ad98ff0e33e59d7ed9ffd18eaf5437f35e17
pg42870 <74028296+pg42870@users.noreply.github.com>
2022-01-06 17:48:39 +0000

    UPDATE

--- src/si/supervised/logreg.py
+++ src/si/supervised/logreg.py
@@ -54,14 +54,24 @@ class LogisticRegression(Model):
 			res = 0
 		return res
 
-	def cost(self, X=None):
-		X = add_intersect(X) if X is not None else self.X
+	#def cost(self, X=None):
+		#X = add_intersect(X) if X is not None else self.X
 		#y = y if y is not None else self.Y
 		#theta = theta if theta is not None else self.theta
 		#y_pred = np.dot(self.X, self.theta)
 		#return mse(y, y_pred) / 2
-		y_pred = np.dot(X, self.theta)
-		return mse(self.Y, y_pred) / 2
+		#y_pred = np.dot(X, self.theta)
+		#return mse(self.Y, y_pred) / 2
+
+	def cost(self, X=None, y=None, theta=None):
+		X = add_intersect(X) if X is not None else self.X
+		y = y if y is not None else self.y
+		theta = theta if theta is not None else self.theta
+		m, n = X.shape
+		h = sigmoid(np.dot(X, theta))
+		cost = (-y * np.log(h) - (1 - y) * np.log(1 - h))
+		res = np.sum(cost) / m
+		return res
 
 
 class LogisticRegressionReg:
