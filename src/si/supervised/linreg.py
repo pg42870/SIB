da006be54682a8fb851864af317595df94de53d3
pg42870 <74028296+pg42870@users.noreply.github.com>
2022-01-06 16:19:32 +0000

    UPDATE

--- src/si/supervised/linreg.py
+++ src/si/supervised/linreg.py
@@ -1,14 +1,97 @@
+from numpy.core.numeric import identity
+from .model import Model
+from ..util.metrics import mse
+import numpy as np
+
+class LinearRegression(Model):
+
+    def __init__(self, gd = False, epochs = 1000, lr = 0.001):
+        '''Linear regression Model
+        epochs: number of epochs
+        lr: learning rate for GD
+        '''
+        super(LinearRegression,self).__init__()
+        self.gd = gd
+        self.theta = None
+        self.epochs = epochs
+        self.lr = lr
+
+    def fit(self, dataset):
+        X, Y = dataset.getXy()
+        X = np.hstack(
+            (np.ones((X.shape[0], 1)), X))  # acrescentar o nosso x só com 1 que corresponde ao termo independente
+        self.X = X
+        self.Y = Y
+        # Closed form or GD
+        self.train_gd(X, Y) if self.gd else self.train_closed(X, Y)  # implement closed train form (see notes)
+        self.is_fitted = True
+
+    def cost(self):
+        y_pred = np.dot(self.X, self.theta)
+        return mse(self.Y, y_pred) / 2
+
+    def train_closed(self, X, Y):
+        '''uses closed form linear algebra to fit the model.
+        theta=inv(XT*X)*XT*y
+        '''
+        self.theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(Y)
+
+    def train_gd(self, X, Y):
+        m = X.shape[0]
+        n = X.shape[1]
+        self.history = {}
+        self.theta = np.zeros(n)
+        for epoch in range(self.epochs):
+            grad = 1 / m * (X.dot(self.theta) - Y).dot(X)  # gradient by definition
+            self.theta -= self.lr * grad
+            self.history[epoch] = [self.theta[:], self.cost()]
+
+    def predict(self, X):
+        assert self.is_fitted, "Model must be fit before predicting"
+        _x = np.hstack(([1], X))
+        return np.dot(self.theta, _x)
+
+class LinearRegressionReg(LinearRegression):
+
+    def __init__(self, gd=False, epochs=100, lr=0.01, lbd=1):
+        '''Linear regression model with L2 regularization
+        :param bool gd: if True uses gradient descent (GD) to train the model
+                        otherwise closed form lineal algebra. Default False.
+        :param int epochs: Number of epochs for GD
+        :param int lr: Learning rate for GD
+        :param float lbd: lambda for the regularization'''
+        super(LinearRegressionReg, self).__init__(gd=gd, epochs=epochs, lbd=lbd)
+
+ def train_closed(self, X, Y):
+        '''Use closed form linear algebra to fit the model.
+        theta=inv(XT*X+lbd*I')*XT*y'''
+        n=X.shape[1]
+        dentity = np.eye(n)
+        dentity[0,0]=0
+        self.theta = np.linalg.inv(X.T.dot(X)+self.lbd*identity).dot(x.T).dot(Y)
+        self.is_fitted = True
 
 
-class LineRegressionReg:
+def train_gd(self, X, Y):
+    '''Uses gradient descent to fit the model'''
+    m = X.shape[0]
+    n = X.shape[1]
+    self.history = {}
+    self.theta = np.zeros(n)
+    lbds = np.full(m, self.lbd)
+    lbds[0] = 0  # so that theta(0) is excluded from regularization form
+    for epoch in range(self.epochs):
+        grad = (X.dot(self.theta) - Y).dot(X)  # gradient by definition
+        self.theta -= (self.lr / m) * (lbds + grad)
+        self.history[epoch] = [self.theta[:], self.cost()]
 
-    def __init__(self):
+
+def predict(self, X):
+    assert self.is_fitted, "Model must be fit before predicting"
+    _x = np.hstack(([1], X))
+    return np.dot(self.theta, _x)
 
 
-    def cost(self, X=None, y=None, theta = None):
-        X =  add_itersect(X) if X is not None else self.X
-        y = y if y is not None else self.y
-        theta = theta if theta is not None else self.theta
-
-        y_pred = np.dot(X,theta)
-        return mse(y, y_pred)/2
+def cost(self):
+    y_pred = np.dot(self.X, self.theta)
+    return mse(self.Y, y_pred) / 2
