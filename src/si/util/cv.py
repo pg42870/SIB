72727c1e789075985cd10fe83aed8d7a78cecf5e
pg42870 <74028296+pg42870@users.noreply.github.com>
2022-01-17 01:21:51 +0000

    UPDATE

--- src/si/util/cv.py
+++ src/si/util/cv.py
@@ -14,7 +14,7 @@ class CrossValidationScores:
         self.train_scores = None
         self.test_scores = None
         self.ds = None
-
+"""
     def run(self):
         train_scores = []
         test_scores = []
@@ -25,20 +25,36 @@ class CrossValidationScores:
             self.model.fit(train)
             train_scores.append(self.model.cost())
             test_scores.append(self.model.cost(test.X, test.Y))
-            #if not self.score:
-                #train_scores.append(self.model.cost())
-                #test_scores.append(self.model.cost(test.X, test.y))
-            #else:
-               # y_train = np.ma.apply_along_axis(self.model.predict,
-                #                                 axis=0,
-                 #                                arr = train.X.T)
-                #train_scores.append(self.score(train.Y,y_train))
-                #y_test = np.ma.apply_along_axis(self.model.predict,
-                #                                axis=0, arr= train.X.T)
-                #test_scores.append(self.score(test.Y, y_test))
         self.train_scores = train_scores
         self.test_scores = test_scores
         self.ds = ds
+        return train_scores, test_scores
+"""
+    def run(self):
+        train_scores = []
+        test_scores = []
+        ds = []
+        true_Y, pred_Y = [], []
+        for _ in range(self.cv):
+            train, test = train_test_split(self.dataset, self.split)
+            ds.append((train, test))
+            self.model.fit(train)
+            if not self.score:
+                train_scores.append(self.model.cost())
+                test_scores.append(self.model.cost(test.X, test.Y))
+                pred_Y.extend(list(self.model.predict(test.X)))
+            else:
+                y_train = np.ma.apply_along_axis(self.model.predict, axis=0, arr=train.X.T)
+                train_scores.append(self.score(train.Y, y_train))
+                y_test = np.ma.apply_along_axis(self.model.predict, axis=0, arr=test.X.T)
+                test_scores.append(self.score(test.Y, y_test))
+                pred_Y.extend(list(y_test))
+            true_Y.extend(list(test.Y))
+        self.train_scores = train_scores
+        self.test_scores = test_scores
+        self.ds = ds
+        self.true_Y = np.array(true_Y)
+        self.pred_Y = np.array(pred_Y)
         return train_scores, test_scores
 
 
