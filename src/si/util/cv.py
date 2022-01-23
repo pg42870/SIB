5cbf3b61783a337226152aa9b22700de2a0e95d1
pg42870 <74028296+pg42870@users.noreply.github.com>
2022-01-23 19:21:18 +0000

    UPDATE

--- src/si/util/cv.py
+++ src/si/util/cv.py
@@ -1,17 +1,18 @@
-from si.util.util import train_test_split, add_intersect
+from ..util.util import train_test_split, add_intersect
 import numpy as np
 import itertools
 
 all = ['CrossValidationScore', 'GridSearchCV']
 
-class CrossValidationScores:
+class CrossValidationScore:
 
-    def __init__(self, model, dataset, **kwargs):
+    def __init__(self, model, dataset, score=None, **kwargs):
         self.model = model
         self.dataset = dataset
         self.cv = kwargs.get('cv', 3)
+        self.score = score
         self.split = kwargs.get('split', 0.8)
-        self.score = kwargs.get('score', None)
+        #self.score = kwargs.get('score', None)
         self.train_scores = None
         self.test_scores = None
         self.ds = None
@@ -35,51 +36,6 @@ class CrossValidationScores:
         train_scores = []
         test_scores = []
         ds = []
-        true_Y, pred_Y = [], []
-        for _ in range(self.cv):
-            train, test = train_test_split(self.dataset, self.split)
-            ds.append((train, test))
-            self.model.fit(train)
-            if self.score != None:
-                train_scores.append(self.model.cost())
-                test_scores.append(self.model.cost(test.X, test.Y))
-                pred_Y.extend(list(self.model.predict(test.X)))
-            else:
-                train_scores.append(self.model.cost())
-                test_scores.append(self.model.cost(test.X, test.Y))
-            true_Y.extend(list(test.Y))
-        self.train_scores = train_scores
-        self.test_scores = test_scores
-        self.ds = ds
-        self.true_Y = np.array(true_Y)
-        self.pred_Y = np.array(pred_Y)
-        return train_scores, test_scores
-
-
-    def toDataframe(self):
-        import pandas as pd
-        assert self.train_scores and self.test_scores, "Need to run first"
-        return pd.DataFrame({'Train Scores': self.train_scores, 'Test Scores': self.test_scores})
-
-class GridSearchCV:
-
-    def __init__(self, model, dataset, parameters, **kwargs):
-        self.model= model
-        self.dataset = dataset
-        hasparam = [hasattr(self.model, param) for param in parameters] #verify if keys are or not atributes
-        if np.all(hasparam):
-            self.parameters = parameters
-        else:
-            index = hasparam.index(False)
-            keys = list(parameters.keys())
-            raise ValueError(f" Wrong parameters: {keys[index]}")
-        self.kwargs = kwargs
-        self.results = None
-
-    def run(self):
-        train_scores = []
-        test_scores = []
-        ds = []
         for _ in range(self.cv):
             train, test = train_test_split(self.dataset, self.split)
             ds.append((train, test))
@@ -97,6 +53,41 @@ class GridSearchCV:
         self.ds = ds
         return train_scores, test_scores
 
+
+    def toDataframe(self):
+        import pandas as pd
+        assert self.train_scores and self.test_scores, "Need to run first"
+        return pd.DataFrame({'Train Scores': self.train_scores, 'Test Scores': self.test_scores})
+
+class GridSearchCV:
+
+    def __init__(self, model, dataset, parameters, score=None, **kwargs):
+        self.model = model
+        self.dataset = dataset
+        self.score = score
+        hasparam = [hasattr(self.model, param) for param in parameters] #verify if keys are or not atributes
+        if np.all(hasparam):
+            self.parameters = parameters
+        else:
+            index = hasparam.index(False)
+            keys = list(parameters.keys())
+            raise ValueError(f" Wrong parameters: {keys[index]}")
+        self.kwargs = kwargs
+        self.results = None
+
+    def run(self):
+        self.results = []
+        attrs = list(self.parameters.keys())
+        values = list(self.parameters.values())
+        from itertools import product
+        for comb in list(product(*values)):
+            for attr, value in zip(attrs, comb):
+                setattr(self.model, attr, value)
+            cv = CrossValidationScore(self.model, self.dataset, self.score, **self.kwargs)
+            cv.run()
+            self.results.append(cv.run())
+        return self.results
+
     #def toDataframe(self):
         #import panda as pd
         #assert self.results, "The grid search needs to be ran."
@@ -112,7 +103,7 @@ class GridSearchCV:
 
     def toDataframe(self):
         import pandas as pd
-        #assert self.results, 'The grid search needs to be ran first'
+        assert self.results, 'The grid search needs to be ran first'
         data = dict()
         for i, k in enumerate(self.parameters.keys()):
             v = list()
