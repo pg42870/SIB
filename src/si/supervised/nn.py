e10e36c36cb98aac849c711da4f8e4cca0a06762
pg42870 <74028296+pg42870@users.noreply.github.com>
2022-01-23 18:47:40 +0000

    UPDATE

--- src/si/supervised/nn.py
+++ src/si/supervised/nn.py
@@ -1,11 +1,15 @@
-import numpy as np
-
 from .model import Model
 from scipy import signal
+
 from abc import ABC, abstractmethod
+from typing import MutableSequence
+import numpy as np
+
+from numpy.core.fromnumeric import size, transpose
+from scipy.signal.ltisys import LinearTimeInvariant
 
 from ..util.metrics import mse, mse_prime
-
+from ..util.im2col import pad2D, im2col, col2im
 
 class Layer(ABC):
     def __init__(self):
@@ -25,7 +29,7 @@ class Dense(Layer):
     def __init__(self, input_size, output_size):
         """Fully connect layer"""
         self.weights = np.random.rand(input_size, output_size) - 0.5
-        self.bias = np.zeros((1,output_size))
+        self.bias = np.zeros((1, output_size))
 
     def setWeights(self, weights, bias):
         if (weights.shape != self.weights.shape):
@@ -63,7 +67,7 @@ class Activation(Layer):
     def forward(self, input_data):
         self.input = input_data
         self.output = self.activation(self.input)
-        return self.input
+        return self.output
 
     def backward(self, output_error, learning_rate):
         #learning_rate is not used because there is no 'lerarnable' parameters
@@ -118,19 +122,25 @@ class NN(Model):
             output = layer.forward(output)
         return output
 
-    def cost(self, X=None, Y=None):
+    def cost(self, X=None, y=None):
         assert self.is_fitted, 'Model must be fit before predict'
         X = X if X is not None else self.dataset.X
-        Y = Y if Y is not None else self.dataset.Y
+        y = y if y is not None else self.dataset.y
         output = self.predict(X)
-        return self.loss(Y, output)
+        return self.loss(y, output)
 
 
-class Pooling2D():
+class Pooling2D(Layer):
 
-    def __init__(self):
-        return
+    def __init__(self, size, stride):
+        self.size = size
+        self.stride = stride
 
+    def pool(X_col):
+        raise NotImplementedError
+
+    def dpool(dX_col,dout_col,pool_cache):
+        raise NotImplementedError
 
     def forward(self, input):
         self.X_shape = input.shape
@@ -165,3 +175,92 @@ class Pooling2D():
 
         return dX
 
+class MaxPoling(Layer):
+    def __init__(self, region_shape):
+        self.region_shape = region_shape
+        self.region_h, self.region_w = region_shape
+
+    def forward(self,input_data):
+        self.X_input = input_data
+        _, self.input_h, self.input_w, self.input_f = input_data.shape
+
+        self.out_h = self.input_h // self.region_h
+        self.out_w = self.input_w // self.region_w
+        output = np.zeros((self.out_h, self.out_w, self.input_f))
+
+        for image, i, j in self.iterate_regions():
+            output[i, j] = np.amax(image)
+        return output
+
+    def backward(self,output_error, lr):
+        pass
+
+    def iterate_regions(self):
+        for i in range(self.out_h):
+            for j in range(self.out_w):
+                image = self.X_input[(i * self.region_h): (i * self.region_h + 2), (j * self.region_h):(j * self.region_h + 2)]
+                yield image, i, j
+
+
+class Conv2D(Layer):
+    def __init__(self, input_shape, kernel_shape, layer_depth, stride=1, padding=0):
+        self.input_shape = input_shape
+        self.in_ch = input_shape[2]
+        self.out_ch = layer_depth
+        self.stride = stride
+        self.padding = padding
+        self.weights = np.random.rand(kernel_shape[0], kernel_shape[1],
+                                      self.in_ch, self.out_ch) - 0.5
+
+        self.bias = np.zeros((self.out_ch, 1))
+
+    def forward(self, input_data):
+        s = self.stride
+        self.X_shape = input_data.shape
+        _, p = pad2D(input_data, self.padding, self.weights.shape[:2], s)
+
+        pr1, pr2, pc1, pc2 = p
+        fr, fc, in_ch, out_ch = self.weights.shape
+        n_ex, in_rows, in_cols, in_ch = input_data.shape
+
+        # compute the dimensions of the convolution output
+        out_rows = int((in_rows + pr1 + pr2 - fr) / s + 1)
+        out_cols = int((in_cols + pc1 + pc2 - fc) / s + 1)
+
+        # convert X and w into the appropriate 2D matrices and take their product
+        self.X_col, _ = im2col(input_data, self.weights.shape, p, s)
+        W_col = self.weights.transpose(3, 2, 0, 1).reshape(out_ch, -1)
+
+        output_data = (W_col @ self.X_col + self.bias).reshape(out_ch, out_rows, out_cols, n_ex).transpose(3, 1, 2, 0)
+        return output_data
+
+    def backward(self, output_error, learning_rate):
+        fr, fc, in_ch, out_ch = self.weights.shape
+        p = self.padding
+        db = np.sum(output_error, axis=(0, 1, 2))
+        db = db.reshape(out_ch, )
+        dout_reshaped = output_error.transpose(1, 2, 3, 0).reshape(out_ch, -1)
+        dW = dout_reshaped @ self.X_col.T
+        dW = dW.reshape(self.weights.shape)
+
+        W_reshape = self.weights.reshape(out_ch, -1)
+        dX_col = W_reshape.T @ dout_reshaped
+        input_error = col2im(dX_col, self.X_shape, self.weights.shape, (p, p, p, p), self.stride)
+
+        self.weights -= learning_rate * dW
+        self.bias -= learning_rate * db
+        return input_error
+
+    def predict(self, input_data):
+        assert self.is_fitted, 'Model must be fit before predicting'
+        output = input_data
+        for layer in self.layers:
+            output = layer.forward(output)
+        return output
+
+    def cost(self, X=None, y=None):
+        assert self.is_fitted, 'Model must be fit before predicting'
+        X = X if X is not None else self.dataset.X
+        y = y if y is not None else self.dataset.y
+        output = self.predict(X)
+        return self.loss(y, output)
