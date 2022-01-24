ae8f1ba1f45f2f684e275d1479b0c6f1dc8848e5
pg42870 <74028296+pg42870@users.noreply.github.com>
2022-01-24 11:26:31 +0000

    UPDATE docstrings

--- src/si/unsupervised/kmeans.py
+++ src/si/unsupervised/kmeans.py
@@ -1,6 +1,5 @@
 import numpy as np
-from si.util.util import l2_distance
-from copy import copy
+from ..util.util import l2_distance
 
 
 class KMeans:
@@ -14,24 +13,23 @@ class KMeans:
     def fit(self, dataset):
         """randomly select k centroids"""
         x = dataset.X
+
         self._min = np.min(x, axis=0)
         self._max = np.max(x, axis=0)
 
     def init_centroids(self, dataset):
-        """
-        iniciar os centroides
-        geramos um np array onde os valores serao gerados de forma randomica de modo a terem uma distribuicao uniforme
-
+        """ Initiate the centroids
+        gets the np array where the values will the randomly generated to be in a normal distribution
         """
         x = dataset.X
+
         self.centroids = np.array(
             [np.random.uniform(
                 low=self._min[1], high=self._max[i], size=(self.k,)
             ) for i in range(x.shape[1])]).T
 
-
     def get_closest_centroid(self, x):
-        "retorna o id do centroide mais proximo"
+        """returns the centroid id of the closest centroid"""
         dist = self.distance(x, self.centroids)
         closest_centroids_index = np.argmin(dist, axis=0)
         return closest_centroids_index
@@ -39,27 +37,26 @@ class KMeans:
     def transform(self, dataset):
         self.init_centroids(dataset)
         print(self.centroids)
+
         X = dataset.X
         changed = True
         count = 0
         old_idxs = np.zeros(X.shape[0])
+
         while changed and count < self.max_iter:
-            idxs = np.apply_along_axis(self.get_closest_centroid, axis=0, arr=X.T) #aplica a funcao get_closest_centroid a todo o x
+            idxs = np.apply_along_axis(self.get_closest_centroid, axis=0,
+                                       arr=X.T)  # applies get_closest_centroid for all the x
+
             cent = []
             for i in range(self.k):
                 cent.append(np.mean(X[idxs == i], axis=0))
+
             self.centroids = np.array(cent)
             count += 1
-            changed = not np.all(old_idxs == idxs) #testa se todos os argumentos sao True
+            changed = not np.all(old_idxs == idxs)  # tests if all arguments are True
             old_idxs = idxs
         return self.centroids, old_idxs
 
-
-    def fit_transform(self,dataset):
+    def fit_transform(self, dataset):
         self.fit(dataset)
         return self.transform(dataset)
-
-
-
-
-
--- src/si/unsupervised/pca.py
+++ src/si/unsupervised/pca.py
@@ -10,20 +10,24 @@ class PCA:
     def fit(self):
         pass
 
-
     def transform(self, dataset):
-        scaled_feature = sc().fit_transform(dataset).X.T #normalizar com o StandardScaler
+        scaled_feature = sc().fit_transform(dataset).X.T  # normalize with StandardScaler
+
         if self.using == "svd":
             self.vectors, self.values, vh = np.linalg.svd(scaled_feature)
         else:
             covariance_matrix = np.cov(scaled_feature.T)  # matriz da covariancia
             self.vectors, self.values = np.linalg.eig(covariance_matrix)
 
-        self.idxs = np.argsort(self.vectors)[::-1]  #idxs das colunas ordenadas por importância de compontes
-        self.eigen_vec, self.eigen_val = self.vectors[self.idxs], self.values[:, self.idxs]  # colunas dos valores e dos vetores são reordenadas pelos idxs das colunas
+        # idxs of the sorted columns by feature importance
+        self.idxs = np.argsort(self.vectors)[::-1]
+
+        # columns of the values and vectors are reorganized by the columns idxs
+        self.eigen_vec, self.eigen_val = self.vectors[self.idxs], self.values[:,
+                                                                  self.idxs]
+
         projection_matrix = (self.eigen_vect.T[:][:self.n_comp]).T
         return scaled_feature.T.dot(projection_matrix)
-
 
     def variance_explained(self):
         variance_explained = []
@@ -33,4 +37,3 @@ class PCA:
 
     def fit_transform(self, dataset):
         return self.transform(dataset)
-
